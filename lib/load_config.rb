04e629f1064b9c69979f6b52fb5e2732cdaac9e2
Rob Esris <resris@asheavenue.com>
2015-04-15 21:59:00 -0400

    Read in numeric params

--- lib/load_config.rb
+++ lib/load_config.rb
@@ -15,10 +15,19 @@ class Config < OpenStruct
   def parse
     File.read(@file_path).each_line do |line|
     case line
-      when /\s*\[(\w+)\]\s*/
-        @current_group = $1.to_sym
-        self[@current_group] = OpenStruct.new
-      else
+      when /^\s*\[(\w+)\]\s*$/
+        @current_group = OpenStruct.new
+        self[$1.to_sym] = @current_group
+      when /^\s*(\D\w*)\s*=\s*(\w+)\s*$/
+        key = $1.to_sym
+        value = $2
+        case value
+        when /^\d+$/
+          value = value.to_i
+        when /^\d*\.\d+$/
+          value = value.to_f
+        end
+        @current_group[key] = value
       end
     end
   end
