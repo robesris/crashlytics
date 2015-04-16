086600f1f34a8e23e13cdff7593a5125b4ab96a5
Rob Esris <resris@asheavenue.com>
2015-04-15 22:34:13 -0400

    Support for Boolean values

--- lib/load_config.rb
+++ lib/load_config.rb
@@ -12,6 +12,9 @@ class Config < OpenStruct
   SYMMETRIC_QUOTED_STRING =  /^\s*(\D\w*)\s*=\s*("')([^"']*)\2\s*$/
   ASYMMETRIC_QUOTED_STRING = /^\s*(\D\w*)\s*=\s*“([^”]*)”\s*$/
   ARRAY =                    /^\s*(\D\w*)\s*=\s*(.*,.*)$/
+  BOOLEAN =                  /^\s*(\D\w*)\s*=\s*(no|yes|true|false)\s*$/i
+  TRUE_VALUES = ['yes', 'true']
+  FALSE_VALUES = ['no', 'false']
 
   def initialize(file_path)
     super()
@@ -26,6 +29,9 @@ class Config < OpenStruct
       when GROUP
         @current_group = OpenStruct.new
         self[$1.to_sym] = @current_group
+      when BOOLEAN
+        value = $2
+        @current_group[$1.to_sym] = TRUE_VALUES.include?(value)
       when SYMMETRIC_QUOTED_STRING
         @current_group[$1.to_sym] = $3
       when ASYMMETRIC_QUOTED_STRING
